4651897d956b67db3413225feed9f8a14b44f73e
rasyidannas <rasshit.dsgn@gmail.com>
2024-12-27 13:39:09 +0700

    Chapter 4 - rule 3 target another frame using link

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,6 +5,13 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first) # Reference to the first fixture quote
   end
 
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
   test "Creating a new quote" do
     # When we visit the Quotes#index page
     # we excpect to see a title with "Quotes" 
@@ -12,13 +19,13 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     # When we click on the link with the text "New Qoute"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
     # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
+    click_on "New quote"
     fill_in "Name", with: "Capybara quote"
+
+    # and we click on "Create Quote"
+    # we expect to land on a page with the title "New quote"
+    assert_selector "h1", text: "New quote"
     click_on "Create quote"
 
     # We expect to be back on the page with the title "Quotes"
@@ -27,21 +34,14 @@ class QuotesTest < ApplicationSystemTestCase
     assert_text "Capybara quote"
   end
 
-  test "Showing a quote" do
-    visit quotes_path
-    click_link @quote.name
-
-    assert_selector "h1", text: @quote.name
-  end
-
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
   
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
