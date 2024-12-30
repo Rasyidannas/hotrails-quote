13530130793520ad438dc31f6dd4638f29600b95
rasyidannas <rasshit.dsgn@gmail.com>
2024-12-30 10:30:16 +0700

    Chapter 5 - Making broadcasting asynchronous with ActiveJob

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,10 +3,10 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
   # we can do shotcut above like this
-  # after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
 
-  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
   after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
