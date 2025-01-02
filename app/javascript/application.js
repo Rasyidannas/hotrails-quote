686b79ad5f97108927aaf6ef76c8ed92379ea4a5
rasyidannas <rasshit.dsgn@gmail.com>
2025-01-02 13:12:52 +0700

    Chapter 7 - Flash messages with Hotwire on the #create action

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,5 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 // This is for disable Turbo on the whole application
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+//import { Turbo } from "@hotwired/turbo-rails"
+//Turbo.session.drive = false
